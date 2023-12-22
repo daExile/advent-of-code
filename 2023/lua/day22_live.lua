618a4ededf91ed4fb5791cd10dd4055757d23ac2
daExile <121984897+daExile@users.noreply.github.com>
2023-12-22 15:11:40 +0700

    2023 day 22 table keys refresh for readability :)

--- 2023/lua/day22_live.lua
+++ 2023/lua/day22_live.lua
@@ -1,11 +1,11 @@
-local bricks = {}
+local function comp(b1, b2) return b1.z1 < b2.z1 end
 
-local function comp(b1, b2) return b1[5] < b2[5] end
+local bricks = {}
 for line in io.lines("../__in/22.txt") do
-    local coords = {}
-    for n in string.gmatch(line, "([%d]+)") do table.insert(coords, tonumber(n)) end
-                        -- {x1, x2, y1, y2, z1, z2, supported by, supports}
-    table.insert(bricks, {coords[1], coords[4], coords[2], coords[5], coords[3], coords[6], {}, {}})
+    local xyz = {}
+    for n in string.gmatch(line, "([%d]+)") do table.insert(xyz, tonumber(n)) end
+    
+    table.insert(bricks, {x1 = xyz[1], x2 = xyz[4], y1 = xyz[2], y2 = xyz[5], z1 = xyz[3], z2 = xyz[6], sup_by = {}, sups = {}})
 end
 
 table.sort(bricks, comp)
@@ -13,49 +13,47 @@ table.sort(bricks, comp)
 local floor = {}
 for id, b in ipairs(bricks) do   -- very primitive stacking by checking each tile, performs fine, so be it
     local z_support, supported_by = 0, {}
-    for x = b[1], b[2] do
-        for y = b[3], b[4] do
+    for x = b.x1, b.x2 do
+        for y = b.y1, b.y2 do
             if not floor[x] then floor[x] = {} end; if not floor[x][y] then floor[x][y] = {} end
             local z = floor[x][y][1]; if z and z > z_support then z_support = z end end end
             
-    b[5], b[6] = z_support + 1, b[6] - b[5] + z_support + 1
-    for x = b[1], b[2] do
-        for y = b[3], b[4] do
+    b.z1, b.z2 = z_support + 1, b.z2 - b.z1 + z_support + 1
+    for x = b.x1, b.x2 do
+        for y = b.y1, b.y2 do
             if floor[x][y][1] == z_support then supported_by[floor[x][y][2]] = true end
-            floor[x][y] = {b[6], id} end end
+            floor[x][y] = {b.z2, id} end end
     
-    for k, _ in pairs(supported_by) do table.insert(bricks[k][8], id); table.insert(bricks[id][7], k) end
+    for k, _ in pairs(supported_by) do table.insert(bricks[k].sups, id); table.insert(bricks[id].sup_by, k) end
 end
 
 local quickened_disintegrate = 0
 for _, b in ipairs(bricks) do
-    if #b[8] == 0 then quickened_disintegrate = quickened_disintegrate + 1
+    if #b.sups == 0 then quickened_disintegrate = quickened_disintegrate + 1
     else
         local safe = 1
-        for _, b_sup in ipairs(b[8]) do if #bricks[b_sup][7] == 1 then safe = 0 end end
+        for _, b_above in ipairs(b.sups) do if #bricks[b_above].sup_by == 1 then safe = 0 end end
         quickened_disintegrate = quickened_disintegrate + safe
     end
 end
 
-print(string.format("Part 1: %d", quickened_disintegrate))
-
 local maximised_disintegrate = 0
 for id = 1, #bricks do
     local goners, goners_set, check = {id}, {[id] = true}, {}
-    for _, b_next in ipairs(bricks[id][8]) do table.insert(check, b_next) end
+    for _, b_above in ipairs(bricks[id].sups) do table.insert(check, b_above) end
     
     while #check > 0 do
         local recheck = {}
         
         for _, b in ipairs(check) do
             local not_safe = true
-            for _, support in ipairs(bricks[b][7]) do if not goners_set[support] then not_safe = false end end
+            for _, support in ipairs(bricks[b].sup_by) do if not goners_set[support] then not_safe = false end end
             
             if not_safe and not goners_set[b] then  -- check for bricks sent to recheck more than once
                 table.insert(goners, b)
                 goners_set[b] = true
                 
-                for _, b_next in ipairs(bricks[b][8]) do table.insert(recheck, b_next) end
+                for _, b_above in ipairs(bricks[b].sups) do table.insert(recheck, b_above) end
             end
         end
         check = recheck
@@ -64,4 +62,5 @@ for id = 1, #bricks do
     maximised_disintegrate = maximised_disintegrate + (#goners - 1)
 end
 
+print(string.format("Part 1: %d", quickened_disintegrate))
 print(string.format("Part 2: %d", maximised_disintegrate))
